7c46bdebe109a7e75f580ad2b2d55757c89c84a1
Shio3001 <taku2000maru@icloud.com>
2025-03-03 23:01:13 +0900

    fix : tree

---
+++ src/analysis_interface.cpp
@@ -0,0 +1,45 @@
+#ifndef ANALYSIS_INTERFACE_HPP
+#define ANALYSIS_INTERFACE_HPP
+
+#include "./lexical/lexical_analysis.hpp"
+#include "./syntactic/syntactic_analysis.hpp"
+#include "./definition.hpp"
+#include "./lr_table/lr_table.hpp"
+#include "./BNF/bnf.hpp"
+#include "./input_table.hpp"
+
+// source_code_file_name
+// table_file_name
+// bytecode
+Syntactic::SyntacticTree analysisInterface(string source_code_file_name, string table_file_name)
+{
+    char *source_code = new char[source_code_size * sizeof(char)];
+
+    loadText(source_code, source_code_file_name.c_str(), source_code_size);
+    vstring token_class_type = {};
+    LexicalAnalysis::vLexicalToken token_string_vector = LexicalAnalysis::lexSyntax(source_code, token_class_type);
+
+    free(source_code);
+
+    LRTable::LRTableMultilayer LR_table_multilayer;
+    inputTable(table_file_name, LR_table_multilayer);
+
+    printf("syntacticAnalysis\n");
+
+    LRTable::vReduceFormula syntactic_analysis_formula = {};
+    Syntactic::syntacticAnalysis(LR_table_multilayer, token_string_vector, syntactic_analysis_formula);
+
+    printf("syntacticPurseTree\n");
+    Syntactic::vSyntacticTree syntactic_analysis_tree = {};
+
+    Syntactic::syntacticAnalysisTree(syntactic_analysis_formula, syntactic_analysis_tree, token_string_vector);
+    Syntactic::vSyntacticTree cst = syntactic_analysis_tree;
+    Syntactic::vSyntacticTree ast = {};
+    Syntactic::syntacticParseTree(cst, ast);
+
+    Syntactic::SyntacticTree syntactic_tree(ast);
+
+    return syntactic_tree;
+}
+
+#endif
---
+++ src/syntactic/syntactic_analysis.cpp
@@ -0,0 +1,60 @@
+
+#include "./syntactic_analysis.hpp"
+
+namespace Syntactic
+{
+    int SyntacticTree::getChildrenNodeIndex(int request_node_index, int request_children_index)
+    {
+        if (request_node_index < 0 || request_node_index >= tree.size())
+        {
+            // 例外スロー
+            printf("error : %d is out of range (tree)\n", request_node_index);
+            exit(1);
+        }
+        SyntacticTreeNode current_node = tree[request_node_index];
+
+        if (request_children_index < 0 || request_children_index >= current_node.children.size())
+        {
+            // 例外スロー
+            printf("error : %d is out of range (children)\n", request_children_index);
+            exit(1);
+        }
+        int i = current_node.children[request_children_index];
+        return i;
+    }
+
+    SyntacticTreeNode SyntacticTree::getChildrenNode(int request_node_index, int request_children_index)
+    {
+        if (request_node_index < 0 || request_node_index >= tree.size())
+        {
+            // 例外スロー
+            printf("error : %d is out of range (tree)\n", request_node_index);
+            exit(1);
+        }
+        SyntacticTreeNode current_node = tree[request_node_index];
+
+        if (request_children_index < 0 || request_children_index >= current_node.children.size())
+        {
+            // 例外スロー
+            printf("error : %d is out of range (children)\n", request_children_index);
+            exit(1);
+        }
+
+        SyntacticTreeNode c = tree[current_node.children[request_children_index]];
+        return c;
+    }
+
+    SyntacticTreeNode SyntacticTree::getNode(int request_node_index)
+    {
+        if (request_node_index < 0 || request_node_index >= tree.size())
+        {
+            // 例外スロー
+            printf("error : %d is out of range\n", request_node_index);
+            exit(1);
+        }
+
+        SyntacticTreeNode current_node = tree[request_node_index];
+        return current_node;
+    }
+
+}
